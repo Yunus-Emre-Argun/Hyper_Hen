0b651c680e6d6275f6013df72f392b8dc735dad8
aayberk7 <ayberkk7535@gmail.com>
2025-05-01 23:27:14 +0300

    a

---
+++ chapter.py
@@ -0,0 +1,15 @@
+from enemy import Enemy
+
+class ChapterOne:
+
+    def __init__(self):
+        self.enemy=Enemy()
+        self.missionPass=False
+
+
+class ChapterTwo:
+    def __init__(self):
+        self.enemy1=Enemy()
+        self.enemy2=Enemy()
+        self.missionPass=False
+
